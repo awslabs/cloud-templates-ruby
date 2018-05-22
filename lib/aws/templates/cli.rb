e8dd559ed9de03cd589f463e04aa9d9d01190ccb
Ivan Matylitski <buffovich@gmail.com>
2018-05-22 16:45:54 +0100

    Explain error message with attached causes
    Because we've changed how exceptions handle details attached to
    them and we changed the details themselves, we need to unwind
    exception causes ourselves

--- lib/aws/templates/cli.rb
+++ lib/aws/templates/cli.rb
@@ -8,6 +8,19 @@ module Aws
     module Cli
       def self.start(*args)
         Cli::Interface.start(*args)
+      rescue StandardError => e
+        explain_error(e)
+        exit 1
+      end
+
+      def self.explain_error(exception)
+        cursor = exception
+
+        while cursor
+          puts cursor
+          puts
+          cursor = cursor.cause
+        end
       end
     end
   end
